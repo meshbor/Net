1e24aa80b015fff53c64c3c909ae420a1560681b
Boris Mescheriakov <67239265+meshbor@users.noreply.github.com>
2020-10-29 02:02:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Template} from './components/template'
+
+function App() {
+  return (
+    <div className="App">
+    <Template/>
+    </div>
+  );  
+}
+
+export default App;
